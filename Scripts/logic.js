1ef31c7c9322a26a272bada9ef6ea1c3677f9351
maizedaniele <danielecaschili@tiscali.it>
2018-06-26 22:34:52 +0200

    Updated cursor
    Cursor updates hoovering day recap

--- Scripts/logic.js
+++ Scripts/logic.js
@@ -12,19 +12,25 @@ $("#day3").click(function(){
 
 
 $("#day3").hover(function(){
-    $("#day3").css("box-shadow", "2px 2px 2px 2px #FFA000, -2px -2px 2px 2px #FFA000");    
+    $("#day3").css("box-shadow", "2px 2px 2px 2px #FFA000, -2px -2px 2px 2px #FFA000"); 
+    document.documentElement.style.cursor = "pointer"   
 }, function(){
     $("#day3").css("box-shadow", "0px 0px #FFA000");
+    document.documentElement.style.cursor = "default"   
 });
 
 $("#day2").hover(function(){
     $("#day2").css("box-shadow", "2px 2px 2px 2px #FFA000, -2px -2px 2px 2px #FFA000");
+    document.documentElement.style.cursor = "pointer"  
 }, function(){
     $("#day2").css("box-shadow", "0px 0px #FFA000");
+    document.documentElement.style.cursor = "default"  
 });
 
 $("#day1").hover(function(){
     $("#day1").css("box-shadow", "2px 2px 2px 2px #FFA000, -2px -2px 2px 2px #FFA000");
+    document.documentElement.style.cursor = "pointer"  
 }, function(){
     $("#day1").css("box-shadow", "0px 0px #FFA000");
+    document.documentElement.style.cursor = "default"  
 });
